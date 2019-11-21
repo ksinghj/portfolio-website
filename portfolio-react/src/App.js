60da883b1c118c44a55662ce7e6e30511f8e5c71
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-11-21 22:15:49 +0000

    commit to deploy

---
+++ portfolio-react/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./styles/App.css";
+import "./styles/styles.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// Main flex items
+import Designer from "./components/Designer";
+import Developer from "./components/Developer";
+import Creator from "./components/Creator";
+// Tiles
+import DesignerTiles from "./components/DesignerTiles";
+import DeveloperTiles from "./components/DeveloperTiles";
+import CreatorTiles from "./components/CreatorTiles";
+
+function App() {
+  return (
+    <Router>
+      <div className="App container">
+        <Navbar></Navbar>
+        <div className="main-flex">
+          <Route exact path="/" component={Designer} />
+          <Route exact path="/" component={Developer} />
+          <Route exact path="/" component={Creator} />
+        </div>
+        <Route path="/DesignerTiles" component={DesignerTiles} />
+        <Route path="/DeveloperTiles" component={DeveloperTiles} />
+        <Route path="/CreatorTiles" component={CreatorTiles} />
+        <Footer></Footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
