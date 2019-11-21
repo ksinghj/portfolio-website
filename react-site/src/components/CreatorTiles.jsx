af5b26177a13f5a69aa8c589261fa28182e94b83
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-11-21 17:25:59 +0000

    v1 release

--- react-site/src/components/CreatorTiles.jsx
+++ react-site/src/components/CreatorTiles.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../styles/tiles.css";
+// img
+import questation from "../img/tiles/designer/Questation.png";
 
 class CreatorTiles extends Component {
   render() {
     return (
-      <div className="creator-tiles">
-        <div className="tile-nav">
-          <Link to="/">Back to start</Link>
-          <Link to="/DesignerTiles">
-            <button class="landing-button blue">
-              Designer<i class="fas fa-angle-right"></i>
-            </button>
-          </Link>
+      <React.Fragment>
+        <div className="creator-tiles">
+          <div className="tile-nav">
+            <Link to="/">
+              <button className="landing-button blue">
+                <i className="fas fa-angle-left"></i>Back to start
+              </button>
+            </Link>
+            <Link to="/DesignerTiles">
+              <button class="landing-button blue">
+                Designer<i class="fas fa-angle-right"></i>
+              </button>
+            </Link>
+          </div>
         </div>
 
-        <div className="tile">Project</div>
-        <div className="tile">Project</div>
-        <div className="tile">Project</div>
-        <div className="tile">Project</div>
-      </div>
+        <div className="page-name">
+          <h1>Creator</h1>
+          <p className="recent">Recent work examples</p>
+        </div>
+
+        <div className="designer-tiles tiles">
+          <div className="tile tile-1">
+            <img src={questation} alt="My maths app" />
+            <p className="name">Questation</p>
+            <p className="desc">
+              Coming soon... An app built for maths teachers, currently in
+              development.
+            </p>
+            <button className="landing-button red">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://google.com/"
+              >
+                See more here
+              </a>
+            </button>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
