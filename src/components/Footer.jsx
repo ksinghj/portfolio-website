0aac550b339a606ef6a46ca700d8c06673c37553
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-11-21 22:18:17 +0000

    deploy attempt 3

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return <footer>&copy;Kartar Singh Jabanda 2019</footer>;
+  }
+}
+
+export default Footer;
