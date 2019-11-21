a6b8fcb42667e9da49f3c51155146cb84414f5d3
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-11-21 22:22:20 +0000

    rm unneccesary folders

--- portfolio-react/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Footer extends Component {
-  render() {
-    return <footer>&copy;Kartar Singh Jabanda 2019</footer>;
-  }
-}
-
-export default Footer;
